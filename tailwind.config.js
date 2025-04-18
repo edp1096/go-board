a0a91b26b24f4553a9a6190060b87347d0442ec9
edp1096 <edp1096@naver.com>
2025-04-18 16:55:11 +0900

    dark - try

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,12 @@
-{}
+// tailwind.config.js
+module.exports = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            boxShadow: {
+                // 커스텀 그림자 정의
+                'dark': '0 1px 3px 0 rgba(255, 255, 255, 0.1), 0 1px 2px 0 rgba(255, 255, 255, 0.06)',
+            }
+        }
+    }
+}
